573f8d138a7ebada991302b0c97355128cc8f446
msxshankar <92645384+msxshankar@users.noreply.github.com>
2022-04-19 22:39:58 +0100

    Added a simple menu system
    Menu system allows the option to update packages or exit.
    If a incorrect value is entered, the user is prompted to try again.

--- update.sh
+++ update.sh
@@ -1,7 +1,19 @@
-#!/bin/bash                                                                                                           
+#!/bin/bash                                                                                
 
 # Program to update system packages
  
+# Menu System
+startMenu () {
+cat <<- _EOF_
+	A simple package update tool
+
+	1. Upgrade packages
+	2. Quit
+	
+_EOF_
+
+read -p "Enter selection [1-2] > "
+}
 
 # Updates, upgrades and cleans pop os/ubuntu repos
 apt_update () {
@@ -11,7 +23,6 @@ apt_update () {
 	return	
 }
   
-
 # Updates, upgrades and cleans flatpaks
 flatpak_update () {
 	flatpak update -y
@@ -19,32 +30,52 @@ flatpak_update () {
 	return
 }
 
-
 # Updates the pop-os recovery partition
 popRecovery_update () {
 	pop-upgrade recovery upgrade from-release
 	return
 }
 
-# Finds the distro name
-if [ -f /etc/os-release ]; then
-	. /etc/os-release
-	OS=$NAME
-fi
+# Updates the correct packages for the distro
+updatePackages () {
 
-# Runs appropriate command for distro
-if [ "$NAME" = "Pop!_OS" ]; then
-	apt_update
-	
-	# Asks user if they want to update pop-os recovery partition
-	read -p "Do you want to upgrade the recovery partition? (y/n) "
-	
-	if [ "$REPLY" = "y" ]; then
-		popRecovery_update
+	# Finds the distro name
+	if [ -f /etc/os-release ]; then
+		. /etc/os-release
+		OS=$NAME
 	fi
-fi
 
-# Updates flatpak if installed
-if [ "$(which flatpak)" = /usr/bin/flatpak ]; then
-	flatpak_update
-fi
+	# Runs appropriate command for distro
+	if [ "$NAME" = "Pop!_OS" ]; then
+		apt_update
+		
+		# Asks user if they want to update pop-os recovery partition
+		read -e -p "Do you want to upgrade the recovery partition? (y/n) " -i "n"
+		
+		if [ "$REPLY" = "y" ]; then
+			popRecovery_update
+		fi
+	fi
+
+	# Updates flatpak if installed
+	if [ "$(which flatpak)" = /usr/bin/flatpak ]; then
+		flatpak_update
+	fi
+}
+
+
+# Starts menu system
+condition=1
+while [ "$condition" -eq 1 ]; do
+	startMenu
+
+	if [ "$REPLY" = 1 ]; then
+		updatePackages
+		exit 0
+	elif [ "$REPLY" = 2 ]; then
+		exit 0
+	else 
+		echo -e "Please try again\n"
+		sleep 1
+	fi
+done
