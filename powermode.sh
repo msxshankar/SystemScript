a18bbd167844e4a79eb30dc8edf5d7161ec91308
Mayur <92645384+msxshankar@users.noreply.github.com>
2022-07-22 12:02:56 +0100

    Add powermode.sh for Lenovo Ideapad 14ARE05

---
+++ powermode.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Power management script for the Lenovo Ideapad 14ARE05 using ACPI
+
+# Menu-System
+
+startMenu () {
+clear
+cat <<- _EOF_
+	Power Management Utility for the Lenovo Ideapad 14ARE05
+
+	1. Change laptop performance power mode
+	2. Change battery saver mode
+	3. Change rapid charge mode
+	4. Exit 
+_EOF_
+read -p "Enter selection [1-4] > "
+}
+
+powermodeMenu () {
+clear
+cat <<- _EOF_
+	Power Managent Utility for the Lenovo Ideapad 14ARE05
+
+	Choose performance mode:
+	1. Battery mode
+	2. Intelligent mode
+	3. Extreme mode
+	4. Go back
+	5. Exit
+_EOF_
+read -p "Enter selection [1-5] > "
+}
+
+batterysaverMenu () {
+clear
+cat <<- _EOF_
+	Power Managent Utility for the Lenovo Ideapad 14ARE05
+
+	Turn battery saver mode on or off
+	1. On (Caps battery at 60%)
+	2. Off
+	4. Go back
+	5. Exit
+_EOF_
+read -p "Enter selection [1-5] > "
+}
+
+rapidchargeMenu () {
+clear
+cat <<- _EOF_
+	Power Managent Utility for the Lenovo Ideapad 14ARE05
+
+	Turn rapid charge mode on or off
+	1. On (Charges more quickly but may wear out battery)
+	2. Off
+	4. Go back
+	5. Exit
+_EOF_
+read -p "Enter selection [1-5] > "
+}
+
+
+VAR=1
+DELAY=1
+
+while [[ "$VAR" == 1 ]]; do
+
+	startMenu
+
+	# Validating user input
+	if [[ "$REPLY" =~ ^[1-4]$ ]]; then
+		if [[ "$REPLY" == 4 ]]; then
+			echo "Exiting..."
+			exit 1
+		fi
+
+		if [[ "$REPLY" == 1 ]];then
+			powermodeMenu	
+			
+		fi
+
+		if [[ "$REPLY" == 2 ]];then
+			batterysaverMenu
+		fi
+
+		if [[ "$REPLY" == 3 ]];then
+			rapidchargeMenu
+		fi
+	else
+		echo "Invalid entry. Try again."
+		sleep "$DELAY"
+	fi
+done
+
+
+
